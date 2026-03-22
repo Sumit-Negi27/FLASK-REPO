a4b89df94c04f5f8dae87267b6b3924db9c4f52c
Sumit Negi <negi759123@gmail.com>
2026-03-22 23:46:28 +0530

    update

--- Phase 2/app.py
+++ Phase 2/app.py
@@ -4,4 +4,7 @@ app= Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("form.html")
+@app.route("/submit", method=["POST"])
+def login():
+    return render_template("form.html")
